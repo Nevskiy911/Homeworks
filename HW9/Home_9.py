7e89250f170a8918125082358d788d9c21935dbc
Oleksandr Malieiev <maleev1820@gmail.com>
2023-03-27 22:14:19 +0200

    Update Home_9.py

--- HW9/Home_9.py
+++ HW9/Home_9.py
@@ -15,7 +15,6 @@ def input_error(func):
             return "Not enough user name or phone number. Try again"
     return inner
 
-
 def hello(*args):
     return """How can I help you?"""
 
@@ -24,22 +23,27 @@ def help(*args):
     for key, values in COMMANDS.items():
         print(f"Command: {values}")
 
-
 @input_error
 def add(*args):
-    phone_book[args[0]] = args[1]
-    phones = args[1]
-    name = args[0]
-    if not phones:
-        raise ValueError()
-    return f"{name} phones: {phones} has added"
+    data = args[0].split(" ")
+    name = data[0].strip()
+    phone = data[1].strip()
+    phone_book[name] = phone
+    return f"{name} phones: {phone} has added"
 
+@input_error
+def change(*args):
+    data = args[0].split(" ")
+    name = data[0].strip()
+    num = data[1].strip()
+    if name in phone_book.keys():
+        phone_book[name] = num
+    return f"{name} number has been changed for number: {num}"
 
 def show_all(*args):
     for k, v in phone_book.items():
         print(f"{k}: {v}")
     return hello()
-
 
 @input_error
 def phone(*args):
@@ -47,41 +51,28 @@ def phone(*args):
         if v in args:
             print(f"{k}: {v}")
 
-
-
-
 def exit(*args):
     return "Good bye!"
-
-
-def close(*args):
-    return "Good bye!"
-
-
-def good_bye(*args):
-    return "Good bye!"
-
 
 def no_command(*args):
     return "Unknown command, try again!"
 
 
-COMMANDS = {help: "help",
-            add: "add",
-            exit: "exit",
-            close: "close",
-            good_bye: "good bye",
-            show_all: "show all",
-            phone: "phone",
-            hello: "hello"}
+COMMANDS = {"help": help,
+            "add": add,
+            "show all": show_all,
+            "phone": phone,
+            "hello": hello,
+            "exit": exit,
+            "close": exit,
+            "good bye": exit}
 
 
 def command_handler(text):
     for command, kword in COMMANDS.items():
-        if text.startswith(kword):
-            return command, text.replace(kword, "").strip()
+        if text.startswith(command):
+            return kword, text.replace(command, "").strip()
     return no_command, None
-
 
 def main():
     print(hello())
@@ -89,7 +80,7 @@ def main():
         user_input = input(">>> ")
         command, data = command_handler(user_input)
         print(command(data))
-        if command == exit or command == close or command == good_bye:
+        if user_input in ["exit", "close", "good bye"]:
             break
 
 
